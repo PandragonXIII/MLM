5efd23ebf9cb26feb99d765a5c46ad75d70273fe
XIII <XIII@126.com>
2024-04-09 13:42:28 +0800

    visualization of denoised img

--- utils.py
+++ utils.py
@@ -2,6 +2,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import pandas as pd
+import joypy
 
 def show_sampled_pic(path):
     # read the npz file and show the sampled images
@@ -23,7 +25,6 @@ def show_sampled_pic(path):
     for i in range(n):
         plt.imshow(samples[i])
         plt.show()
-
 
 def img_to_array(path:str):
     # convert image under the path dir to array and save them as one .npz file, 
@@ -50,8 +51,54 @@ def array_to_img(path:str):
         plt.imsave(f"E:\\research\\MLLM\\src\\image\\denoised\\{name}_{i}.bmp", samples[i])
     return
 
+
+def cos_sim_distribution(path:str):
+    '''input: cosine similarity csv file 
+            rows: text
+            cols: image
+        output: None
+        save the visualization of the cosine similarity distribution
+    '''
+    # read the csv file
+    df = pd.read_csv(path)
+    df.drop(["text"],inplace=True, axis=1)
+    print(f"shape: {df.shape}")
+
+    pic_types = df.columns
+
+    # plot the distribution, x-axis: cossim, y-axis: frequency
+    nplot = df.shape[1]
+    # Draw Plot
+    fig, axes = joypy.joyplot(df,
+                            colormap=plt.cm.get_cmap("Spectral", nplot),
+                            figsize=(10, 6))
+    # plt.show()
+    plt.savefig(".\\src\\results\\cos_sim_of_mix_text.png")
+
+    # create a new dataframe with columns: adversarial, benign, pic_type
+    # and the value is the cosine similarity, with the corresponding type
+    df2 = pd.DataFrame(columns=["adversarial", "benign", "pic_type"])
+    for i in range(df.shape[1]):
+        adversarial = df.iloc[:40,i]
+        benign = df.iloc[40:,i].reset_index(drop=True)
+        # get a 40 row , 2 col dataframe
+        temp = pd.DataFrame({"adversarial":adversarial, "benign":benign, "pic_type":[pic_types[i]]*40})
+        df2 = pd.concat([df2, temp])
+    # Draw Plot
+    plt.figure(figsize=(16,10), dpi= 80)
+    fig, axes = joypy.joyplot(df2,
+                            by="pic_type",
+                            column=['adversarial', 'benign'],
+                            colormap=plt.cm.get_cmap("Spectral", nplot),
+                            figsize=(10, 6))
+    
+    # plt.show()
+    plt.savefig(".\\src\\results\\cos_sim_of_sep_text.png")
+    return
+
 if __name__ == "__main__":
     # img_to_array(".\\src\\image")
     # show_sampled_pic("src\image\prompt_constrained_16.npz")
     # show_sampled_pic("src\samples\samples_1x256x256x3_pandas16.npz")
-    array_to_img("src\samples\samples_4x256x256x3.npz")
+    # array_to_img("src\samples\samples_4x256x256x3.npz")
+    cos_sim_distribution(".\\src\\analysis\\cosine_similarity.csv")
